dfa8452dcf77320d52d390d18e73552e33b8ba8e
nms377 <nms377@nyu.edu>
2017-01-12 21:04:50 -1000

    Error handling.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function clientReceiveMessage( messgae ){
       if( players.has(msg.payload.username) ){
         // player name is taken
         const error = `username: '${msg.payload.username}' is not available.`;
-        this.send(OP.create(OP.ERROR, { error }));
+        this.sendOp(OP.ERROR, { error });
       } else {
         // username is available, register the player
         this.username = msg.payload.username;
@@ -42,18 +42,38 @@ function clientReceiveMessage( messgae ){
       }
     } else {
       const error = `You are not registered yet. Register with OP:REGISTER first.`;
-      this.send(OP.create(OP.ERROR, { error }));
+      this.sendOp(OP.ERROR, { error });
     }
     return; // trap
   }
-  
+}
+
+function clientDisconnect(){
+if( this.username !== null ){
+    if( players.has(this.username) ){
+      players.delete(this.username);
+    }
+  }
+  console.info(`Client username:'${this.username}' has disconnected.`);
+}
+
+// handles errors
+function sendOp(op, payload){
+  this.sendOp(op, payload), error => {
+    if( error !== undefined ){
+      console.error(`Error writing to client socket`, error);
+      clientDisconnect.call(this);
+    }
+  };
 }
 
 wss.on('connection', client => {
 
 	client.username = null;
+	client.sendOp = sendOp;
 
 	client.on('message', clientReceiveMessage.bind(client));
+	client.on('close', clientDisconnect.bind(client));
 
 });
 
