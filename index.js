447a2cdfc2acd786f673ed2fa58381b740f8a4e7
nms377 <nms377@nyu.edu>
2017-01-12 20:56:04 -1000

    Made changes. Added username registery.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const OP = require('./public/js/OP');
 
 const { Server : WebSocketServer } = require('ws');
 const server = require('http').createServer();
@@ -7,18 +8,53 @@ const wss = new WebSocketServer({ server });
 
 const PORT = process.env.PORT || 3000;
 
+// "username" => client
+const players = new Map();
+
 app.get('/api/hello', (req, res) => {
 	const hello = 'world';
 	res.json({hello});
 });
 
+function clientReceiveMessage( messgae ){
+	let msg;
+	try{
+		msg = OP.parse(message);
+	}catch(error){
+		console.error(error);
+		return this.send(OP.create(OP.ERROR, { error }));
+	}
+
+	 // trap unregistered users
+  if( this.username === null ){
+    // wait for OP:REGISTER
+    if( msg.OP === OP.REGISTER ){
+      // add the player to players
+      if( players.has(msg.payload.username) ){
+        // player name is taken
+        const error = `username: '${msg.payload.username}' is not available.`;
+        this.send(OP.create(OP.ERROR, { error }));
+      } else {
+        // username is available, register the player
+        this.username = msg.payload.username;
+        players.set(this.username, this);
+        this.send(OP.create(OP.REGISTERACK));
+      }
+    } else {
+      const error = `You are not registered yet. Register with OP:REGISTER first.`;
+      this.send(OP.create(OP.ERROR, { error }));
+    }
+    return; // trap
+  }
+  
+}
+
 wss.on('connection', client => {
 
-	client.on('message', message => {
-		console.log('received: %s', message);
-	});
+	client.username = null;
 
-	client.send('connected to ws server');
+	client.on('message', clientReceiveMessage.bind(client));
+
 });
 
 server.on('request', app);
